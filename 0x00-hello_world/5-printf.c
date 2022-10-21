12caea701987c908f67c1832846a5a8c38179934
Bamigbade <bytevillart@gmail.com>
2022-10-21 07:21:30 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main - print c with printf
  * Return: 0 as usual
- */
-	int main(void)
-	{
-  	    printf("with proper grammar, but the outcome is a piece of art,\n");
-	    return (0);
-	}
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+       	return (0);
+}
