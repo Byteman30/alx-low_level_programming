1b34265b76795b71c8270f75660e2420e770ebdb
Bamigbade <bytevillart@gmail.com>
2022-10-21 06:56:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print c with printf
+ * Return: 0 as usual
+ */
+
+	int main(void)
+	{
+	printf("with proper grammar, but the outcome is a piece of art\n")		return (0);
+		}
